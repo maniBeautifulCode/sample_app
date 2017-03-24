f7b3ee01e0e247ef3d6fd8ce873141edce8273f2
maniBeautifulCode <mani@beautifulcode.in>
2017-03-24 11:44:50 +0530

    fixing bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,6 +90,17 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
 
   private
 
